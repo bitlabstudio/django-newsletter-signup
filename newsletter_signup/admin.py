9d7fc956208150bc54fe9da765d0b79480a63964
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2016-04-13 10:37:53 +0200

    Improved admins

--- newsletter_signup/admin.py
+++ newsletter_signup/admin.py
@@ -6,8 +6,11 @@ from . import models
 
 class NewsletterSignupAdmin(admin.ModelAdmin):
     """Custom admin for the ``NewsletterSignup`` model."""
-    list_display = ['email', 'user', 'signup_date', 'verification_date',
-                    'verification_token']
+    list_display = [
+        'email', 'first_name', 'last_name', 'source', 'referer', 'signup_date',
+    ]
+    list_filter = ['source', ]
+    search_fields = ['source', 'email', 'first_name', 'last_name', ]
 
 
 admin.site.register(models.NewsletterSignup, NewsletterSignupAdmin)
