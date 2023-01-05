a5f2a069c4681b7c2b3811a2aa60fcbd8e38eb80
Martin Brochhaus <mbrochh@gmail.com>
2023-01-05 10:09:38 +0100

    chore: fixes url import for Django 4

--- newsletter_signup/urls.py
+++ newsletter_signup/urls.py
@@ -1,20 +1,20 @@
 """URLs for the newsletter_signup app."""
-from django.conf.urls import url
+from django.conf.urls import re_path
 
 from . import views
 
 
 urlpatterns = [
-    url(r'^signup/$',
+    re_path(r'^signup/$',
         views.NewsletterSignupView.as_view(),
         name='newsletter_signup'),
-    url(r'^verify/(?P<uuid>.*)/',
+    re_path(r'^verify/(?P<uuid>.*)/',
         views.NewsletterVerifyView.as_view(),
         name='newsletter_verify'),
-    url(r'^unsubscribe/$',
+    re_path(r'^unsubscribe/$',
         views.NewsletterUnsubscribeView.as_view(),
         name='newsletter_unscubscribe'),
-    url(r'^success/$',
+    re_path(r'^success/$',
         views.NewsletterSignupSuccessView.as_view(),
         name='newsletter_signup_success'),
 ]
